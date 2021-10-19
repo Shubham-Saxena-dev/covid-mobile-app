93fad11a00e9a148cda9e080e763f73698cf2a27
Shubham <ShubhamS>
2021-10-19 13:40:49 +0530

    covid app

---
+++ App.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import {  StatusBar } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import {Provider as PaperProvider} from 'react-native-paper';
+ 
+import {theme} from './src/config/theme';
+import {AppRouter} from './src/navigation';
+
+
+const Stack = createStackNavigator();
+
+
+export default function App() {
+  StatusBar.setBackgroundColor('transparent');
+  // StatusBar.setHidden(true)
+  StatusBar.setTranslucent(true)
+
+  
+  
+  return  (
+    <PaperProvider theme={theme}>
+          <NavigationContainer>
+            <AppRouter />
+          </NavigationContainer>
+    </PaperProvider>
+
+  )
+}
